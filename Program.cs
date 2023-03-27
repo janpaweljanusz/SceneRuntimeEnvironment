8b01cad6c2110935fba97b48c956368096dabc4d
jj <jjanusz88@wp.pl>
2023-03-27 16:00:25 +0200

    asynchronous c# <==> js bridge implemented

--- Program.cs
+++ Program.cs
@@ -1,24 +1,43 @@
 ﻿using Microsoft.ClearScript.V8;
 using Utils;
+using ExtensionMethods;
 
-using (var engine = new V8ScriptEngine())
+class Program
 {
-    engine.AddHostType("Console", typeof(Console));
-    var globalObjects = new Globals.GlobalObjects(engine);
-    var moduleInstance = new Globals.Module(engine);
-
-    engine.Script.originalRequire = (Func<string, dynamic>)moduleInstance.Require;
-    engine.Script.module = moduleInstance;
-
-    var jsFileReader = new EmbeddedFileReader();
-    var sandboxedRuntimeFile = jsFileReader.ReadFromEmbeddedFile("SceneRuntimeEnvironment.js.sandboxedRuntime.js");
-    engine.Execute(sandboxedRuntimeFile);
-    var sandboxedRuntimeWrapperFile = jsFileReader.ReadFromEmbeddedFile("SceneRuntimeEnvironment.js.sandboxedRuntimeWrapper.js");
-    engine.Execute(sandboxedRuntimeWrapperFile);
-
-    engine.Script.onStart();
-    for (int i = 1; i <= 3; i++)
+    static void Main(string[] args)
     {
-        engine.Script.onUpdate(i);
+        MainAsync(args).GetAwaiter().GetResult();
+    }
+    /// <summary>
+    /// Handling async functions require using Task and waiting for till it is completed 
+    /// </summary>
+    static async Task MainAsync(string[] args)
+    {
+        using (var engine = new V8ScriptEngine())
+        {
+            // passing classes to JS engine
+            engine.AddHostType("Console", typeof(Console));
+            engine.AddHostType(typeof(TaskToJSPromise));
+            engine.AddHostType(typeof(Task));
+
+            // instantiating global objects (c#/js bridge)
+            var globalObjects = new Globals.GlobalObjects(engine);
+            var moduleInstance = new Globals.Module(engine);
+
+            // js file reader instantiation
+            var jsFileReader = new EmbeddedFileReader();
+
+            // reading JS files
+            engine.Execute(jsFileReader.ReadFromEmbeddedFile("SceneRuntimeEnvironment.js.sandboxedRuntime.js"));
+            engine.Execute(jsFileReader.ReadFromEmbeddedFile("SceneRuntimeEnvironment.js.sandboxedRuntimeWrapper.js"));
+
+            // actuall challange task execution. App will wait till async will end.
+            var jsAsyncToTask = new JSAsyncToTask();
+            await jsAsyncToTask.ToTask(engine.Script.onStartWrapper);
+            for (int i = 1; i <= 3; i++)
+            {
+                await jsAsyncToTask.ToTask(engine.Script.onUpdateWrapper, i);
+            }
+        }
     }
 }
--- js/sandboxedRuntimeWrapper.js
+++ js/sandboxedRuntimeWrapper.js
@@ -1,3 +1,40 @@
-globalThis.onStart = originalRequire('onStart');
-globalThis.onUpdate = originalRequire('onUpdate');
+const onStart = originalRequire('onStart');
+const onUpdate = originalRequire('onUpdate');
 
+/**
+ * wait in miliseconds
+ * @param {number} msec - miliseconds to wait 
+ */
+const waitAsync = async (msec) => {
+    await Task.Delay(msec).ToPromise();
+}
+/**
+ * @access private
+ * @param {*} func 
+ * @param {*} done 
+ * @param {*} dt 
+ * @param {*} waitMiliseconds 
+ */
+const AsyncWrapper = async (func, done, dt, waitMiliseconds)  =>{
+    if (waitMiliseconds > 0){
+        await waitAsync(1000);
+    }
+    await func(dt);
+    done.Set();
+}
+/**
+ * Wrapper on JavaScript sandboxed file onStart for allowing asynchronous from C# context
+ * @param {object} done - an instance of ManualResetEventSlim
+ * @param {string | number | undefined | null} dt - variable passed from 
+ */
+globalThis.onStartWrapper = async (done, dt) => {
+    AsyncWrapper(onStart, done, dt)
+}
+/**
+ * Wrapper on JavaScript sandboxed file onUpdate for allowing asynchronous from C# context
+ * @param {object} done - an instance of ManualResetEventSlim
+ * @param {string | number | undefined | null} dt - variable passed from 
+ */
+globalThis.onUpdateWrapper = async (done, dt) => {
+    AsyncWrapper(onUpdate, done, dt, 1000)
+}
